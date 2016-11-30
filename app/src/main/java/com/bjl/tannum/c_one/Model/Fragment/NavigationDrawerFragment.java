3ea4a7d91f763c9148788a3764377b8a7b9c628d
tannum <tannummail@gmail.com>
2016-12-01 00:52:36 +0700

    add navigation drawer to project

--- app/src/main/java/com/bjl/tannum/c_one/Model/Fragment/NavigationDrawerFragment.java
+++ app/src/main/java/com/bjl/tannum/c_one/Model/Fragment/NavigationDrawerFragment.java
@@ -1,10 +1,16 @@
 package com.bjl.tannum.c_one.Model.Fragment;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,13 +27,31 @@ import java.util.List;
  */
 public class NavigationDrawerFragment extends Fragment {
 
+    public static final String PREF_FILE_NAME = "testpref";
+    public static final String KEY_USER_LEARN_DRAWER = "user_learned_drawer";
+
     private RecyclerView recyclerView;
     private NavRecyclerviewAdapter adapter;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private boolean mUserLearnedDrawer;
+    private boolean mFromSaveInstanceState;
+    private View containerView;
 
     public NavigationDrawerFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mUserLearnedDrawer = Boolean.valueOf(readFromPreferences(getActivity(),KEY_USER_LEARN_DRAWER,"false"));
+        if(savedInstanceState != null){
+            mFromSaveInstanceState = true;
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -55,6 +79,54 @@ public class NavigationDrawerFragment extends Fragment {
             data.add(info);
         }
         return data;
+    }
+
+
+    public void setUp(int fragmentId, DrawerLayout drawerLayout, Toolbar toolbar) {
+
+        containerView = getActivity().findViewById(fragmentId);
+        mDrawerLayout = drawerLayout;
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(),drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close){
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                if(!mUserLearnedDrawer){
+                    mUserLearnedDrawer = true;
+                    saveToPrefereces(getActivity(),KEY_USER_LEARN_DRAWER,mUserLearnedDrawer+"");
+                }
+                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getActivity().invalidateOptionsMenu();
+
+            }
+        };
+        if(!mUserLearnedDrawer && !mFromSaveInstanceState){
+            mDrawerLayout.openDrawer(containerView);
+        }
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mDrawerToggle.syncState();
+            }
+        });
+    }
+
+    public void saveToPrefereces(Context context, String preferenceName, String preferenceValue){
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME,context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(preferenceName,preferenceValue);
+        editor.commit();
+    }
+    public static String readFromPreferences(Context context , String preferenceName,String defaultValue){
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME,context.MODE_PRIVATE);
+        return sharedPreferences.getString(preferenceName,defaultValue);
     }
 
 }
--- app/src/main/java/com/bjl/tannum/c_one/View/MainActivity.java
+++ app/src/main/java/com/bjl/tannum/c_one/View/MainActivity.java
@@ -1,11 +1,13 @@
 package com.bjl.tannum.c_one.View;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 
+import com.bjl.tannum.c_one.Model.Fragment.NavigationDrawerFragment;
 import com.bjl.tannum.c_one.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Mask: setup tool bar
         toolbar = (Toolbar)findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //Mask: setup navigation drawer
+        NavigationDrawerFragment drawderFragment = (NavigationDrawerFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawderFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout)findViewById(R.id.drawer_layout),toolbar);
+
+
     }
 
     @Override
