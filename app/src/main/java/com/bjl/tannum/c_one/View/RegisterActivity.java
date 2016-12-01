09bed3f0099b7be19ba627a0c4240ff6cddc52c5
tannum <tannummail@gmail.com>
2016-12-01 23:55:04 +0700

    version 1.03 Add : Register Activity

---
+++ app/src/main/java/com/bjl/tannum/c_one/View/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.bjl.tannum.c_one.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bjl.tannum.c_one.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
