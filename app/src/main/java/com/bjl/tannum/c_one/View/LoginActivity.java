3d077cfab4f012b0f02f1a9a571697d30471e009
tannum <tannummail@gmail.com>
2016-12-02 09:15:41 +0700

    version 1.04 Add -> forgot buttn Add -> resgister page

--- app/src/main/java/com/bjl/tannum/c_one/View/LoginActivity.java
+++ app/src/main/java/com/bjl/tannum/c_one/View/LoginActivity.java
@@ -2,14 +2,44 @@ package com.bjl.tannum.c_one.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.bjl.tannum.c_one.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button btnForgot;
+    Button btnLogin;
+    Button btnSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnForgot = (Button) findViewById(R.id.btnForgotPwd);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnSignup = (Button)findViewById(R.id.btnSignUp);
+        btnForgot.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+        btnSignup.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if(view == btnForgot){
+            Log.d("debug","btn forgot was clicked");
+        }
+        else if(view == btnLogin){
+            Log.d("debug","btn login was clicked");
+        }
+        else if(view == btnSignup){
+            Log.d("debug","btn signup was clicked");
+        }
+
+
     }
 }
